c8650b4a83a3fa087f60e55e3c42ea24652ef116
d-zer <ozerd689@gmail.com>
2022-03-14 15:04:58 +0300

    algoritma sorulari

---
+++ odev/odev_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace odev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bir konsol uygulamasında kullanıcıdan pozitif iki sayı girmesini isteyin (n, m). 
+            // Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. 
+            // Kullanıcının girmiş olduğu sayılardan m'e eşit yada tam bölünenleri console'a yazdırın. 
+            Console.WriteLine("2. Soru Cözümü");
+            Console.WriteLine("Girilecek sayi adedi: ");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Bölen sayiyi giriniz: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n2; i++)
+            {
+                Console.WriteLine("{0}.Sayıyı Giriniz;", i + 1);
+                int sayi2 = Convert.ToInt32(Console.ReadLine());
+                if ((sayi2 == m) || (sayi2 % m == 0))
+                {
+
+                   Console.WriteLine(sayi2 + ", " + m + "e" + " tam bölünüyor");
+                }
+           }
+        }
+    }
+}
---
+++ program3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace odev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bir konsol uygulamasında kullanıcıdan bir cümle yazması isteyin. 
+            // Cümledeki toplam kelime ve harf sayısını console'a yazdırın.
+            Console.WriteLine("4. Soru Cözümü");
+            Console.WriteLine("Bir Cümle Giriniz ");
+            string cumle = Convert.ToString(Console.ReadLine());
+            string[] kelimeler = cumle.Split(" ");
+            char[] harfler = cumle.ToCharArray();
+
+            int harfSayisi = 0;
+
+            Console.WriteLine("Verilen Cümlede {0} Kelime Vardır.",kelimeler.Length);
+            for (int i = 0; i < kelimeler.Length; i++)
+            {
+                 char[] harfdizi = kelimeler[i].ToCharArray();
+                harfSayisi += harfdizi.Length;
+            }
+            Console.WriteLine("Verilen Cümlede {0} Harf Vardır.",harfSayisi);
+        }
+
+    }
+}
