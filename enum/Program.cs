104931c7390f172c070bde0bbaba23cac6510ad7
d-zer <ozerd689@gmail.com>
2022-03-28 18:50:27 +0300

    struct ve enum kavramlari

---
+++ enum/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace csharp_enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar); // Pazar --> string olarak
+            Console.WriteLine((int)Gunler.Cumartesi); // 24
+
+            int sicaklik = 32;
+
+            if (sicaklik <= (int)HavaDurumu.Normal)
+            {
+                Console.WriteLine("Disariya cikmak icin havanin biraz daha isinmasini bekle");
+            }
+            else if (sicaklik >= (int)HavaDurumu.Sicak)
+            {
+                Console.WriteLine("Disariya cikmak icin cok sicak bir gun");
+            }
+            else if (sicaklik >= (int)HavaDurumu.Normal && sicaklik < (int)HavaDurumu.CokSicak)
+            {
+                Console.WriteLine("Hadi disari cik");
+            }
+        }
+    }
+
+    enum Gunler // numerik veriyi anlamli tutma
+    {
+        Pazartesi = 1, // eleman adini belirtir, index = 0 defaultta
+        Sali, // 2
+        Carsamba, // 3
+        Persembe, // 4
+        Cuma = 23,
+        Cumartesi, // 24
+        Pazar // 25
+    };
+
+    enum HavaDurumu
+    {
+        Soguk = 5,
+        Normal = 20,
+        Sicak = 25,
+        CokSicak = 30
+    }
+}
---
+++ struct_kavrami/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace struct_kavrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dikdortgen dikdortgen = new Dikdortgen(); // new olmazsa olmaz
+            dikdortgen.kisaKenar = 3;
+            dikdortgen.uzunKenar = 4;
+            Console.WriteLine("Class Alan Hesabi: {0}", dikdortgen.AlanHesapla());
+
+            Dikdortgen_Struct dikdortgen_struct = new Dikdortgen_Struct(3, 4); // new olmadan yaratilabilir
+            // Dikdortgen_Struct dikdortgen_struct // initialize degerleri atayamaz --> 16, 17. satir yazilmali
+            // dikdortgen_struct.kisaKenar = 3;
+            // dikdortgen_struct.uzunKenar = 4;
+            Console.WriteLine("Struct Alan Hesabi: {0}", dikdortgen_struct.AlanHesapla());
+        }
+    }
+
+    class Dikdortgen
+    {
+        public int kisaKenar;
+        public int uzunKenar;
+        public Dikdortgen()
+        {
+            kisaKenar = 3;
+            uzunKenar = 4;
+        }
+        public long AlanHesapla()
+        {
+            return this.kisaKenar * this.uzunKenar;
+        }
+    }
+
+    struct Dikdortgen_Struct
+    {
+        public int kisaKenar;
+        public int uzunKenar;
+        public Dikdortgen_Struct(int kisaKenar, int uzunKenar)
+        {
+            // kisaKenar = 3;
+            // uzunKenar = 4;
+            this.kisaKenar = kisaKenar;
+            this.uzunKenar = uzunKenar;
+        } // parametresi olmayan constructor'i struct icin tanimlayamazsin
+        public long AlanHesapla()
+        {
+            return this.kisaKenar * this.uzunKenar;
+        }
+    }
+}
