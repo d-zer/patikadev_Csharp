1235a2e9abfcd250d98e8f136ea81e92e22c755b
d-zer <ozerd689@gmail.com>
2022-04-06 11:15:53 +0300

    interface otomobil ornegi

---
+++ arayuzler-ornek/Civic.cs
@@ -0,0 +1,23 @@
+namespace arayuzler_ornek
+{
+    public class Civic : IOtomobil
+    {
+        public Marka HangiMarkaninAraci()
+        {
+           // throw new NotImplementedException();
+           return Marka.Honda;
+        }
+
+        public int KacTekerlektenOlusur() // refactor edilebilir
+        {
+           // throw new NotImplementedException();
+           return 4;
+        }
+
+        public Renk StandartRengiNe()
+        {
+           // throw new NotImplementedException();
+           return Renk.Gri;
+        }
+    }
+}
